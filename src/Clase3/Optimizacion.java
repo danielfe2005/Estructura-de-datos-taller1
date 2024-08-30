bdf6d16a9ba34ec5fb56d98bc9eff8b64bd4245b
danielfe2005 <daniel.vargas1@estudiantesunibague.edu.co>
2024-08-29 23:29:36 -0500

    first commit

--- src/Clase3/Optimizacion.java
+++ src/Clase3/Optimizacion.java
@@ -2,31 +2,30 @@ package Clase3;
 
 import java.util.Arrays;
 
-public class Optimizacion
-{
-    public boolean hasDuplicate(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    import java.util.Arrays;
-
-    public boolean hasDuplicate2(int[] arr)  //complejidad 0(n log n)
-    {
-        Arrays.sort(arr);
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == arr[i - 1]) {
-                return true;
-            }
-        }
-
-        return false;
-    }
+public class Optimizacion {
+//    public boolean hasDuplicate(int[] arr) {
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = i + 1; j < arr.length; j++) {
+//                if (arr[i] == arr[j]) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+//
+//    import java.util.Arrays;
+//
+//    public boolean hasDuplicate2(int[] arr)  //complejidad 0(n log n)
+//    {
+//        Arrays.sort(arr);
+//
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i] == arr[i - 1]) {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+//    }
 }
